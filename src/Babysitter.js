d4b4aac28a7f08f85da107300fd107de8eec73de
Jason Dinkelmann <jason@aboutgolf.com>
2016-11-13 22:37:00 -0500

    Validated that the end time is passed

--- src/Babysitter.js
+++ src/Babysitter.js
@@ -1,9 +1,13 @@
 var Babysitter = function (startTime, endTime, bedTime) {
     this.startTime = startTime;
+    this.endTime = endTime;
 
     Babysitter.prototype = {
         getStartTime: function () {
             return this.startTime;
+        },
+        getEndTime: function () {
+            return this.endTime;
         }
     }
 };
--- test/BabysitterSpec.js
+++ test/BabysitterSpec.js
@@ -1,13 +1,22 @@
 describe('Babysitter Kata', function () {
+    var babysitter, startTime,endTime, bedTime;
+    beforeEach(function () {
+        startTime = new Date("November 13, 2016 15:00:00");
+        endTime = new Date("November 14, 2016 03:59:59");
+        bedTime = new Date("November 13, 2016 21:00:00");
 
+        babysitter = new Babysitter(startTime,endTime);
+    });
     it('should define the Babysitter', function () {
         var babysitter = new Babysitter();
         expect(babysitter).toBeDefined();
     });
 
     it('should return a start time given a valid start time', function () {
-        var today = new Date();
-        var babysitter = new Babysitter(today);
-        expect(babysitter.getStartTime()).toEqual(today);
+        expect(babysitter.getStartTime()).toEqual(startTime);
+    });
+
+    it('should retrun the end time if one is passed', function () {
+        expect(babysitter.getEndTime()).toEqual(endTime);
     });
 });
